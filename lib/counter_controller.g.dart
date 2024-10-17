a7cea0ce6b6a68ed3e79da420a3ce84349d81289
Masahiro Aoki <freud427@gmail.com>
2024-10-17 13:27:22 +0900

    add sample

---
+++ lib/counter_controller.g.dart
@@ -0,0 +1,26 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'counter_controller.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$counterControllerHash() => r'd808456bb0647c7517258c84b5152cb2da2464ab';
+
+/// See also [CounterController].
+@ProviderFor(CounterController)
+final counterControllerProvider =
+    AutoDisposeNotifierProvider<CounterController, int>.internal(
+  CounterController.new,
+  name: r'counterControllerProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$counterControllerHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CounterController = AutoDisposeNotifier<int>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
